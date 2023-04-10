bc5765a246ee437cd5829de6a058a64a8eb7683d
sajibsaifullah <dusajibphy@gmail.com>
2023-04-10 23:13:21 +0600

    featured job section edited

--- src/components/FeaturedJob.jsx
+++ src/components/FeaturedJob.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { MapPinIcon } from "@heroicons/react/24/solid";
 
 const FeaturedJob = ({ featuredJob }) => {
-  console.log(featuredJob);
+//   console.log(featuredJob);
   return (
     <div className="border rounded-md pl-8 py-4">
       <img src={featuredJob.company_logo} alt="" />
-      <h3 className="text-xl font-bold">{featuredJob.company_name}</h3>
+      <h3 className="text-xl font-bold my-3">{featuredJob.company_name}</h3>
       <div className="flex gap-8">
-        <p>{featuredJob.remote_or_onsite}</p>
-        <p>{featuredJob.fulltime_or_parttime}</p>
+        <p className="border rounded-sm px-2 text-blue-500">
+          {featuredJob.remote_or_onsite}
+        </p>
+        <p className="border rounded-sm px-2 text-blue-500">
+          {featuredJob.fulltime_or_parttime}
+        </p>
       </div>
-      <div className="flex gap-8">
-        <p>{featuredJob.location}</p>
+      <div className="flex gap-8 my-2">
+        <div className="flex items-center gap-1">
+          <MapPinIcon className="h-4 w-4 text-gray-500" />
+          <p>{featuredJob.location}</p>
+        </div>
         <p>{featuredJob.salary}</p>
       </div>
       <button className="btn bg-purple-500">View Details</button>
--- src/components/FeaturedJobs.jsx
+++ src/components/FeaturedJobs.jsx
@@ -10,7 +10,7 @@ const FeaturedJobs = () => {
         <div className='container mx-auto'>
             <h1 className='text-3xl text-center font-bold mt-12'>Featured Jobs</h1>
             <p className='text-center mt-2'>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            <div className='grid grid-cols-2 gap-5'>
+            <div className='grid md:grid-cols-2 gap-5 mt-5'>
                 {
                     featuredJobs.map(featuredJob => <FeaturedJob 
                         key={featuredJob.id}
