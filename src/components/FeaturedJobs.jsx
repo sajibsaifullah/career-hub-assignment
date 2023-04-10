df5ac307d30d553fc4dc18b247f6457be6fa4050
sajibsaifullah <dusajibphy@gmail.com>
2023-04-10 23:42:24 +0600

    see all button added

--- src/components/FeaturedJobs.jsx
+++ src/components/FeaturedJobs.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import FeaturedJob from './FeaturedJob';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import FeaturedJob from "./FeaturedJob";
 
 const FeaturedJobs = () => {
+  const featuredJobs = useLoaderData();
+  const [showAll, setShowAll] = useState(false);
 
-    const featuredJobs = useLoaderData()
-    // console.log(featuredJobs[0]);
-    return (
-        <div className='container mx-auto'>
-            <h1 className='text-3xl text-center font-bold mt-12'>Featured Jobs</h1>
-            <p className='text-center mt-2'>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            <div className='grid md:grid-cols-2 gap-5 mt-5'>
-                {
-                    featuredJobs.map(featuredJob => <FeaturedJob 
-                        key={featuredJob.id}
-                        featuredJob={featuredJob}
-                    ></FeaturedJob>)
-                }
-            </div>
-        </div>
-    );
+  const hnadleShowAll = () => {
+    setShowAll(true);
+  };
+  // console.log(featuredJobs[0]);
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl text-center font-bold mt-12">Featured Jobs</h1>
+      <p className="text-center mt-2">
+        Explore thousands of job opportunities with all the information you
+        need. Its your future
+      </p>
+      <div className="grid md:grid-cols-2 gap-5 mt-5">
+        {featuredJobs.slice(0, showAll ? 6 : 4).map((featuredJob) => (
+          <FeaturedJob
+            key={featuredJob.id}
+            featuredJob={featuredJob}
+          ></FeaturedJob>
+        ))}
+      </div>
+      <div className="text-center mt-4">
+        {
+            !showAll && <button onClick={hnadleShowAll} className="btn background">See All Jobs</button>
+        }
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedJobs;
