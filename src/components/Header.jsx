2cd982e3849b0cec01ecc2802ac9431d235f533b
sajibsaifullah <dusajibphy@gmail.com>
2023-04-10 14:47:55 +0600

    import assets file & edited header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <h2 className='text-3xl'>This is Header</h2>
+            <Link to="/">Home</Link>
+            <Link to="/statistics">Statistics</Link>
+            <Link to="/appliedJobs">Applied Jobs</Link>
+            <Link to="/blog">Blog</Link>
         </div>
     );
 };
