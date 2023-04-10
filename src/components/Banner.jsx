c126d3afe0f7c0c30b2086ad296b7efd2a4ca292
sajibsaifullah <dusajibphy@gmail.com>
2023-04-10 22:50:21 +0600

    banner added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-// import '../assets/All Images/P3OLGJ1 copy 1.png'
+// import '../assets/Images/profileImg.png';
+import profileImg from '../assets/Images/profileImg.png';
 
 
 const Banner = () => {
-    // const img = 'P3OLGJ1 copy 1.png';
     return (
-        <div>
-            <div>
-                <h1>One Step Closer To Your Dream Job</h1>
-                <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
-                <button>Get Started</button>
+        <div className='grid lg:grid-cols-2 container mx-auto my-8'>
+            <div className='mt-16 ml-8'>
+                <h1 className='text-5xl font-bold'>One Step <br /> Closer To Your <br /> <span className='text-purple-400'>Dream Job</span></h1>
+                <p className='my-8'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                <button className='btn background'>Get Started</button>
             </div>
-            <div>
-                {/* <img src={img} alt="" /> */}
+            <div className='w-[75%] lg:ml-36'>
+                <img src={profileImg} alt="" />
             </div>
 
         </div>
