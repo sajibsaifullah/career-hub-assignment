a079c24d2574e6703eec1d68a052ae8e6a9bb949
sajibsaifullah <dusajibphy@gmail.com>
2023-04-11 00:17:58 +0600

    editted

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const JobList = ({jobList}) => {
     return (
         <div className='border rounded-md pl-12 py-8 mt-4 bg-slate-100'>
-            <img src={jobList.category_logo} alt="" />
+            <img src={jobList.category_logo} alt="logo" />
             <h4 className='text-xl font-bold'>{jobList.category_name}</h4>
             <p>{jobList.jobs_available} Jobs Available</p>
             
