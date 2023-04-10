4bd70f3438ddcc07248428b3764c6ddd1c13293c
sajibsaifullah <dusajibphy@gmail.com>
2023-04-10 15:35:25 +0600

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            <h2 className='text-3xl'>This is Header</h2>
-            <Link to="/">Home</Link>
-            <Link to="/statistics">Statistics</Link>
-            <Link to="/appliedJobs">Applied Jobs</Link>
-            <Link to="/blog">Blog</Link>
+  return (
+    <nav className="container mx-auto">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/statistics">Statistics</Link>
+              </li>
+
+              <li>
+                <Link to="/appliedJobs">Applied Jobs</Link>
+              </li>
+              <li>
+                <Link to="/blog">Blog</Link>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-3xl text-purple-600 font-bold">
+            TalentScout
+          </a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/statistics">Statistics</Link>
+            </li>
+
+            <li>
+              <Link to="/appliedJobs">Applied Jobs</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <Link className="btn bg-purple-500">
+            S<span className="lowercase">tar</span> &nbsp; A
+            <span className="lowercase">pplying</span>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
