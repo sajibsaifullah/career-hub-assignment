9f0af051798ef24826ce5f8e2112f129c5116831
sajibsaifullah <dusajibphy@gmail.com>
2023-04-11 20:40:46 +0600

    editted

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Job = ({job, dynamic}) => {
     // console.log(dynamic.jobId);
     return (
-        <div className='container mx-auto flex flex-col lg:flex-row justify-between gap-8 px-4 pt-4 lg:pt-8'>
+        <div className='container mx-auto flex flex-col lg:flex-row justify-between gap-8'>
             <div className='lg:w-[60%]'>
                 { job.id==dynamic.jobId &&
-                    <div className='pl-3'>
+                    <div className='pl-3 mx-4 mt-4 lg:mt-8'>
                         <p className='py-4'><span className='font-bold'>Job Description: &nbsp;</span> {job.job_description}
                         </p>
                         <p className='pb-4'><span className='font-bold'>Job Responsibility: &nbsp;</span> {job.job_responsibility}
@@ -20,7 +20,7 @@ const Job = ({job, dynamic}) => {
             </div>
             <div className='lg:w-[40%] text-center'>
                 { job.id==dynamic.jobId &&
-                    <div className='border rounded-md bg-gray-100 pl-4 py-6'>
+                    <div className='border rounded-md bg-gray-100 pl-4 py-6 mx-4 mt-4 lg:my-8'>
                         <h1 className='text-xl font-bold mb-6'>Job Details</h1>
                         <p><span className='font-bold'>Salary: &nbsp;</span> {job.salary}</p>
                         <p><span className='font-bold'>Job Title: &nbsp;</span> {job.job_title}</p>
